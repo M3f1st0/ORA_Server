dcc7feecc584d05030c117d264dd8b297c605407
pibit <pibit@debian.matrix>
2016-05-09 19:05:16 +0200

    #SSL Server version 1.0

--- src/ora_server/ConnectionManager.java
+++ src/ora_server/ConnectionManager.java
@@ -5,11 +5,13 @@
  */
 package ora_server;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.*;
+import java.security.Security;
+import java.security.PrivilegedActionException;
+
+import javax.net.ssl.*;
+import com.sun.net.ssl.*;
+import com.sun.net.ssl.internal.ssl.Provider;
 
 /**
  *
@@ -17,22 +19,54 @@ import java.util.logging.Logger;
  */
 public class ConnectionManager {
 
-    private final int port = 8000;
+    private final int intSSLport = 4443;
 
     public void processClient() {
+        // Registering the JSSE provider
+        Security.addProvider(new Provider());
+
+        //Specifying the Keystore details
+        System.setProperty("javax.net.ssl.keyStore", "src/resources/orakeystore.ks");
+        System.setProperty("javax.net.ssl.keyStorePassword", "kspasswd");
+
+        // Enable debugging to view the handshake and communication which happens between the SSLClient and the SSLServer
+        System.setProperty("javax.net.debug","all");
         try {
-            ServerSocket serverSocket = new ServerSocket(port);
-            Socket socket = serverSocket.accept();
-            System.out.println("connected");
-            
-            //do something with client (new thread)
-            
-        } catch (IOException ex) {
-            Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, null, ex);
+            // Initialize the Server Socket
+            SSLServerSocketFactory sslServerSocketfactory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+            SSLServerSocket sslServerSocket = (SSLServerSocket) sslServerSocketfactory.createServerSocket(intSSLport);
+            SSLSocket sslSocket = (SSLSocket) sslServerSocket.accept();
+
+            // Create Input / Output Streams for communication with the client
+            while (true) {
+                PrintWriter out = new PrintWriter(sslSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(
+                                sslSocket.getInputStream()));
+                String inputLine, outputLine;
+
+                while ((inputLine = in.readLine()) != null) {
+                    out.println(inputLine);
+                    System.out.println(inputLine);
+                }
+
+                // Close the streams and the socket
+                out.close();
+                in.close();
+                sslSocket.close();
+                sslServerSocket.close();
+
+            }
+        } catch (Exception exp) {
+            PrivilegedActionException priexp = new PrivilegedActionException(exp);
+            System.out.println(" Priv exp --- " + priexp.getMessage());
+
+            System.out.println(" Exception occurred .... " + exp);
+            exp.printStackTrace();
         }
     }
-    
-    public void connectToDB(){
+
+    public void connectToDB() {
         DBHandler.connect();
     }
 }
